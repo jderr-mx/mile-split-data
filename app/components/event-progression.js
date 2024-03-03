a35aa0f948f31aae1103b99fb07c30f41855088f
John Derr <john.derr@maximos.io>
2024-03-03 18:31:59 -0500

    add progression graph

---
+++ app/components/event-progression.js
@@ -0,0 +1,92 @@
+import Component from '@glimmer/component';
+import * as Plot from '@observablehq/plot';
+
+export default class EventProgressionComponent extends Component {
+  MEASURED_EVENTS = ['D', 'HT', 'WT', 'S', 'J', 'LJ', 'HJ', 'PV'];
+  get plot() {
+    const conversionFunction = this.getUnitConversionFunction();
+    return Plot.plot({
+      marginLeft: 100,
+      marks: [
+        Plot.lineY(this.unitValues, {
+          curve: 'natural',
+          marker: 'circle',
+          x: Plot.indexOf,
+        }),
+        Plot.tip(this.progressionData, { y: 'meetName', x: Plot.indexOf }),
+        Plot.circle(this.prArray, {
+          x: Plot.indexOf,
+          y: 'units',
+          stroke: 'blue',
+          strokeWidth: 2,
+          r: 5,
+        }),
+        Plot.text(this.prArray, {
+          y: 'units',
+          x: Plot.indexOf,
+          text: () => 'PR',
+          stroke: 'blue',
+          strokeWidth: 1,
+          dy: -12,
+        }),
+        Plot.axisX([], { tickFormat: null }),
+      ],
+      y: {
+        domain: this.domain,
+        tickFormat: (d) => conversionFunction(d),
+        tickSize: 0,
+        reverse: this.isTimedEvent,
+      },
+    });
+  }
+  get eventData() {
+    return this.args.eventData;
+  }
+  get progressionData() {
+    return this.eventData.toReversed();
+  }
+
+  get domain() {
+    const min = Math.min(...this.unitValues); //- 24000;
+    const max = Math.max(...this.unitValues); //+ 24000;
+    return [Math.floor(min / 100) * 100, Math.ceil(max / 100) * 100];
+  }
+
+  get unitValues() {
+    return this.progressionData.map((item) => item.units);
+  }
+
+  get prArray() {
+    const prValue = this.isTimedEvent
+      ? Math.min(...this.unitValues)
+      : Math.max(...this.unitValues);
+    return this.unitValues.map((value) =>
+      value == prValue ? { units: value } : { units: null },
+    );
+  }
+
+  unitsToDistance(units) {
+    const remainder = units % 12000;
+    const inches = remainder / 1000;
+    const feet = (units - remainder) / 12000;
+    return `${feet}-${inches}`;
+  }
+
+  unitsToTime(units) {
+    const date = new Date(units);
+    const min = date.getMinutes();
+    const sec = date.getSeconds().toString().padStart(2, '0');
+    const ms = (date.getMilliseconds() / 10).toString().padStart(2, '0');
+    return `${min}:${sec}.${ms}`;
+  }
+
+  getUnitConversionFunction() {
+    if (this.isTimedEvent) return this.unitsToTime;
+    else return this.unitsToDistance;
+  }
+
+  get isTimedEvent() {
+    const eventCode = this.args.eventCode;
+    return !this.MEASURED_EVENTS.includes(eventCode);
+  }
+}
--- mirage/config.js
+++ mirage/config.js
@@ -10,7 +10,7 @@ export default function (config) {
     // Remove discoverEmberDataModels if you do not want ember-cli-mirage to auto discover the ember models
     models: {
       ...discoverEmberDataModels(config.store),
-      ...config.models
+      ...config.models,
     },
     // uncomment to opt into ember-cli-mirage to auto discover ember serializers
     // serializers: applyEmberDataSerializers(config.serializers),
@@ -21,19 +21,17 @@ export default function (config) {
 }
 
 function routes() {
-
+  this.passthrough();
   // These comments are here to help you get started. Feel free to delete them.
-
   /*
     Config (with defaults).
 
     Note: these only affect routes defined *after* them!
   */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
+  this.urlPrefix = 'https://www.milesplit.com';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  this.namespace = 'api/v1'; // make this `/api`, for example, if your API is namespaced
+  this.passthrough();
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
   /*
     Shorthand cheatsheet:
 
